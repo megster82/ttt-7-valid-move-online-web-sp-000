608f780c3a23879769a5e62090b125468df8c5a2
meaghan jennings <meaghanljennings@gmail.com>
2021-02-08 21:00:03 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,25 +1,24 @@
 def valid_move?(board, index)
   def position_taken?(array, ind)
     if array[ind] == " " || array[ind] == "" || array[ind] == nil
-      return false
-    else
+    return false
+  else
+    return true
+  end
+end 
+
+  def on_board?(number)
+    if number.between?(0,8) == true 
       return true
+    else
+      return false
     end
   end
 
-  def on_board?(num)
-    if num.between?(0, 8) == true
-      return true
-    else
-      return false
-    end
-  end
-
-  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+  if on_board?(index) == true && position_taken?(board, index) == false
     return true
   else
     return false
   end
-
-
+  
 end
