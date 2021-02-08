357e8d5a93c47bc34aba63ed8804877781322dc0
meaghan jennings <meaghanljennings@gmail.com>
2021-02-08 20:40:13 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,4 +6,10 @@ def valid_move?(board, index)
   end
 end 
 
-def 
+def position_taken?(board, index)
+  if board[index] == " " || (board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
