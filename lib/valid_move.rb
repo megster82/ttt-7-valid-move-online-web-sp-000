5b95424012a3e0a4a358c56971080edab1c42ef8
meaghan jennings <meaghanljennings@gmail.com>
2021-02-08 20:34:44 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,16 +1,18 @@
-def valid_move?()
-  if index.between?(1,9)
-    return true
-  else
-    return false
-  end
-  
+def valid_move?(board, index)
+
   def position_taken?(board, index)
     if board[index] == " " || (board[index] == "" || board[index] == nil
       return false
     else
       return true
     end
+  end
+  
+  if index.between?(1,9)
+    return true
+  else
+    return false
+  end
 
   if valid_move? && !position_taken?
   return true
